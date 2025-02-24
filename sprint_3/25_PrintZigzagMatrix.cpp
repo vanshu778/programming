6727b93a513851f8de85257c19b1bd779805fa60
vanshu778 <vthesiya778@gmail.com>
2025-02-24 20:11:13 +0530

    problem 21-30

---
+++ sprint_3/25_PrintZigzagMatrix.cpp
@@ -0,0 +1,25 @@
+// 1 2 3 
+// 6 5 4
+// 7 8 9
+#include <iostream>
+using namespace std;
+
+void printZigzagMatrix(int n) {
+    int num = 1;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < n; j++) cout << num++ << " ";
+        } else {
+            num += n - 1;
+            for (int j = 0; j < n; j++) cout << num-- << " ";
+            num += n + 1;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 3;
+    printZigzagMatrix(n);
+    return 0;
+}
