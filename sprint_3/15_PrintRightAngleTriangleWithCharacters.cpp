59333d5f5c827308f17a6f4cd7f3710c4ff21a6c
vanshu778 <vthesiya778@gmail.com>
2025-02-24 19:55:13 +0530

    problem 11-20

---
+++ sprint_3/15_PrintRightAngleTriangleWithCharacters.cpp
@@ -0,0 +1,20 @@
+// A
+// BB
+// CCC
+#include <iostream>
+using namespace std;
+
+void printCharacterTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        char ch = 'A' + i - 1;
+        for (int j = 0; j < i; j++)
+            cout << ch;
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 3;
+    printCharacterTriangle(n);
+    return 0;
+}
---
+++ sprint_3/18_PrintBorderPatternWithNumbers.cpp
@@ -0,0 +1,24 @@
+// 1234
+// 1  4
+// 1  4
+// 1234
+#include <iostream>
+using namespace std;
+
+void printBorderPattern(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+                cout << j + 1;
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 4;
+    printBorderPattern(n);
+    return 0;
+}
