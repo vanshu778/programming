8f4f9bee184d9fba20d42822b5d65a4ee3eefa68
vanshu778 <vthesiya778@gmail.com>
2025-02-24 20:23:31 +0530

    problem 31-40

---
+++ sprint_3/31_PrintTriangularMatrixWithNumbers.cpp
@@ -0,0 +1,21 @@
+// 1 
+// 2 3
+// 4 5 6
+#include <iostream>
+using namespace std;
+
+void printTriangularMatrix(int n) {
+    int num = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << num++ << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 3;
+    printTriangularMatrix(n);
+    return 0;
+}
